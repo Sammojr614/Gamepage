5a409af7a21fc99a11cbf2769104f41bfd71d86a
Sammmojr <sammojr614@gmail.com>
2019-08-29 13:22:41 -0400

    More Canvas Drawing

--- java/main.js
+++ java/main.js
@@ -1,6 +1,5 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-
 //Ground Tile
 ctx.beginPath();
 ctx.fillStyle = "#7cfc00 "
@@ -20,3 +19,20 @@ ctx.fillStyle = "Yellow"
 ctx.fill();
 ctx.stroke();
 
+
+// Might be a Stick figure
+//body
+ctx.beginPath();
+ctx.fillStyle = "black";
+ctx.fillRect(90,120,10,15);
+ctx.stroke();
+//head
+ctx.beginPath();
+ctx.arc(95,115,5,0, 2 * Math.PI);
+ctx.fillStyle = "black";
+ctx.fill();
+ctx.stroke();
+//left arm
+ctx.beginPath();
+ctx.fillRect(85,121,5,10);
+ctx.stroke();
