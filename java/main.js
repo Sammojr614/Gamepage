46619724a3797715b8bae195690b4caca2fde225
Sammojr614 <Sammojr614@gmail.com>
2019-08-29 10:35:30 -0400

    Changed some java and Renamed folder

--- java/main.js
+++ java/main.js
@@ -1,8 +1,4 @@
-var centerX = getWidth()/2;
-var centerY = getHeight()/2;
-function start(){
-    var rec = new Rectangle(100,100);
-    rec.setPosition(0,0);
-    add(rec);
-}
-
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d"); 
+ctx.fillStyle = "#34FFAB"; 
+ctx.fillRect(0,0,200,50);
--- java/works.js
+++
@@ -1,4 +0,0 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d"); 
-ctx.fillStyle = "#34FFAB"; 
-ctx.fillRect(0,0,200,50);
