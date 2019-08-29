53949d11db174e7fa1ede50598a3c704bbcbad38
Sammmojr <sammojr614@gmail.com>
2019-08-29 13:03:02 -0400

    Messing Around with the canvas

--- java/main.js
+++ java/main.js
@@ -1,36 +1,16 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-//First red Rectangle
+//Ground Tile
 ctx.beginPath();
-ctx.fillStyle ="#FF0000";
-ctx.fillRect(0,0,150,50);
-ctx.strokeStyle ="red";
+ctx.fillStyle = "#7cfc00 "
+ctx.fillRect(0,150,200,100);
 ctx.stroke();
 
-//Second red Rectangle
+//sky tile
 ctx.beginPath();
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0,150,150,50);
-ctx.strokeStyle ="red";
+ctx.fillStyle = "SkyBlue"
+ctx.fillRect(0,0, 200,150);
 ctx.stroke();
 
-//First yellow Rectangle
-ctx.beginPath();
-ctx.fillStyle ="#00FF34";
-ctx.fillRect(0,100,50,25);
-ctx.strokeStyle ="yellow";
-ctx.stroke();
 
-//second Yellow Rectangle
-ctx.beginPath();
-ctx.fillStyle ="#00FF34";
-ctx.fillRect(0,75,50,25);
-ctx.strokeStyle ="yelllow"
-ctx.stroke();
-
-//Circle Attept
-ctx.beginPath();
-ctx.fillPath ="#00FF34";
-ctx.arc(95, 95, 45, 0, 2 * Math.PI);
-ctx.stroke();
