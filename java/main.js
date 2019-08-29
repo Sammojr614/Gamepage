8ecfe1013c28200b88ecc5a86b2ecf30f356ce24
Sammmojr <sammojr614@gmail.com>
2019-08-29 11:23:15 -0400

    Finally Got to Draw on the Canvas

--- java/main.js
+++ java/main.js
@@ -1,2 +1,28 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+
+//First red Rectangle
+ctx.beginPath();
+ctx.fillStyle ="FF0000";
+ctx.fillrect(0,0,150,50);
+ctx.strokeStyle ="red";
+ctx.stroke();
+
+//Second red Rectangle
+ctx.beginPath();
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0,150,150,50);
+ctx.strokeStyle ="red";
+ctx.stroke();
+
+//First yellow Rectangle
+ctx.beginPath();
+ctx.rect(0,100,50,25);
+ctx.strokeStyle ="yellow";
+ctx.stroke();
+
+//second Yellow Rectangle
+ctx.beginPath();
+ctx.rect(0,75,50,25);
+ctx.strokeStyle ="yelllow"
+ctx.stroke();
