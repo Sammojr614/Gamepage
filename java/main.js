f4631ccef85420d44c1ce2cc3c817b8fc4533adf
Sammojr614 <Sammojr614@gmail.com>
2019-08-29 10:28:29 -0400

    Java stuff

--- java/main.js
+++ java/main.js
@@ -1,9 +1,5 @@
 var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-<<<<<<< HEAD
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0, 0, 150, 75); 
-=======
+var ctx = canvas.getContext("2d"); 
 ctx.fillStyle = "#34FFAB"; 
 ctx.fillRect(0,0,200,50);
->>>>>>> 22046eb75255016fc7903655ef8715ceb8670ddf
+
