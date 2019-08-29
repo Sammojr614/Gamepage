ab458f5d52ea5943475ad70c3d6a4531cd7e7b15
Sammmojr <sammojr614@gmail.com>
2019-08-29 11:01:02 -0400

    java work

--- java/main.js
+++ java/main.js
@@ -1,6 +1,2 @@
 var canvas = document.getElementById("myCanvas");
-var context = canvas.getContext("2d")
-
-context.beginPath();
-context.lineWidth = "6";
-context.strokeStyle = "red";
+var ctx = canvas.getContext("2d");
