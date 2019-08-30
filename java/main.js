a87ac3f120c901391d3a0095349f759a5ba6e45d
Sammojr614 <Sammojr614@gmail.com>
2019-08-30 10:45:12 -0400

    messing with fun canvas stuff

--- java/main.js
+++ java/main.js
@@ -1,25 +1,13 @@
 var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var ball;
+
+
 start();
 
 function start(){
-        drawCircle(20,"#FF0000", 10, 10);
-}
 
-function drawCircle(radius, color, x, y){
-    var ctx = canvas.getContext("2d");
-
-    ctx.beginPath();
+} 
+function drawCircle(radius, x, y){
     ctx.arc(x, y, radius, 0, 2 * Math.PI);
-    ctx.fillStyle = color;
-    ctx.fill();
-    ctx.stroke();
-
-}
-function checkMouse(){
-    canvas.addEventListener("mousedown", doMouseDown, false);
-}
-function doMouseDown(event){
-    canvas_y = event.page;
-    canvas_x = event.page;
-    alert("X =" + canvas_x + "Y=" +canvas_y);
 }
