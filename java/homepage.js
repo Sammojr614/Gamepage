813d41fc0f7cb80fa2ef2b0e98ac579e3ef6f547
Sammmojr <sammojr614@gmail.com>
2019-08-30 13:39:04 -0400

    Added Java to the Homepage

---
+++ java/homepage.js
@@ -0,0 +1,16 @@
+var canvas = document.getElementById("homepageCanvas");
+var ctx = canvas.getContext("2d");
+
+start();
+
+function start(){
+askForLogin();
+
+}
+
+function askForLogin(){
+    var username = prompt("Username: ");
+    var password = prompt("Password: ");
+    if(username == "Sammojr614" && password == "mario1");
+        alert("Welcome back!");
+    }
