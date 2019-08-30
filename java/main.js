ebca2399fe403d5db0f7c97fe3117be4c719a5d6
Sammojr614 <Sammojr614@gmail.com>
2019-08-30 09:33:09 -0400

    Working With js Functions

--- java/main.js
+++ java/main.js
@@ -1,6 +1,25 @@
 var canvas = document.getElementById("myCanvas");
-var context = canvas.getContext("2d")
+start();
 
-context.beginPath();
-context.lineWidth = "6";
-context.strokeStyle = "red";
+function start(){
+        drawCircle(20,"#FF0000", 10, 10);
+}
+
+function drawCircle(radius, color, x, y){
+    var ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.stroke();
+
+}
+function checkMouse(){
+    canvas.addEventListener("mousedown", doMouseDown, false);
+}
+function doMouseDown(event){
+    canvas_y = event.page;
+    canvas_x = event.page;
+    alert("X =" + canvas_x + "Y=" +canvas_y);
+}
