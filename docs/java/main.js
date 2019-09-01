58f59e1fa41cb7f3fddbea79242db9a682157186
Sammmojr <sammojr614@gmail.com>
2019-09-01 18:14:26 -0400

    Working on Js For gamepage Button

--- docs/java/main.js
+++ docs/java/main.js
@@ -1,33 +1,39 @@
 var canvas = document.getElementById("myCanvas");
+var button = document.getElementById("drawButton");
 var ctx = canvas.getContext("2d");
+button.onclick = function () { buttonDraw() };
 
-start();
 
-function start(){
-    var nameOrRectangle = prompt("Name or Draw a Rectangle?")
-    if(nameOrRectangle == "Name" || nameOrRectangle == "name"){
-        whatIsYourname();
-    }
-    if(nameOrRectangle == "Rectangle" || nameOrRectangle == "rectangle"){
-        drawRect();
-    }   
+function buttonDraw(){
+var nameOrRect = prompt("Want Me to Draw your name? or a Rectangle?: ");
+if(nameOrRect == "Name" || nameOrRect == "name"){
+var name = prompt("What is Your Name?: ");
+ var nameSize = prompt("How big do you want it?: ");
+ var nameFont = prompt("What Font?");
+ var nameColor = prompt("What Color?");
+ drawName(name, nameColor, nameSize, nameFont);
+}
+if(nameOrRect == "Rectangle" || nameOrRect == "rectangle"){
+    var rectHeight = prompt("What Height?: ");
+    var rectWidth = prompt("How wide do you want?: ");
+    var rectColor = prompt("What Color?: ");
+    drawRectangle(rectHeight, rectWidth,rectColor);
 }
 
-function whatIsYourname(){
-    var name = prompt("What is Your name?");
-    var sizeAndFont = prompt("What Size and Font?");
-    var color = prompt("What Color?");
-    ctx.font = sizeAndFont;
-    ctx.textAlign ="center";
-    ctx.fillStyle = color;
-    ctx.fillText(name, 200, 200);
 }
-function drawRect(){
-    var height = prompt("What is the height?");
-    var width = prompt("How wide do you want it?");
-    var color = prompt("What Color? make Sure its Hex code or Rgb")
+function drawName(name, color, size, font){
+ ctx.beginPath();
+ ctx.font = size, font;
+ ctx.fillFont(name);
+ ctx.stroke();
+}
+
+function drawRectangle(height, width, color){
     ctx.beginPath();
     ctx.fillStyle = color;
-    ctx.fillRect(200, 200, width,height);
+    ctx.Fillrect(200,200,width, height);
     ctx.stroke();
+    
+    
 }
+
