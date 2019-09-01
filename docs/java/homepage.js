69b07d50cfff854a32dcd8fff7d7a36094f0a301
Sammmojr <sammojr614@gmail.com>
2019-08-31 21:00:53 -0400

    Changes to Login stuff

--- docs/java/homepage.js
+++ docs/java/homepage.js
@@ -11,16 +11,16 @@ function start(){
 function askForLogin(){
  var user_name = prompt("Username: ");
  var password = prompt("Password: ");
+ if(user_name == "Sammojr614" && password == "DevPassWord"){
+    alert("Welcome back, Sam!");
+}
+if(user_name == "Sammojr614" && password != "DevPassword"){
+    alert("Incorrect Password.");
+}
+
+if(user_name == "Holt" && password == "JamieRules"){
+    alert("Welcome Back, Jamison");
+}
  document.cookie = username;
  document.cookie = password;
- if(user_name == "Sammojr614" && password == "DevPassWord"){
-     alert("Welcome back, Sam!");
- }
- if(user_name == "Sammojr614" && password != "DevPassword"){
-     alert("Incorrect Password.");
- }
-
- if(user_name == "Holt" && password == "JamieRules"){
-     alert("Welcome Back, Jamison");
- }
 }
