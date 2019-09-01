d0970620149aee855f7a949a86ad66ca952d2d3a
Sammmojr <sammojr614@gmail.com>
2019-08-31 20:55:07 -0400

    Working On login

--- docs/java/homepage.js
+++ docs/java/homepage.js
@@ -1,19 +1,24 @@
 var canvas = document.getElementById("homepageCanvas");
 var ctx = canvas.getContext("2d");
-var username = prompt("Username: ");
-var password = prompt("Password: ");
 var user;
-document.cookie = username;
+
 start();
 
 function start(){
-askForLogin();
-
+    askForLogin();
 }
 
 function askForLogin(){
-    if(username == "Sammojr614" && password == "DevPassCode");
-        alert("Welcome back, Sam!");
-        var user = Sam
-    }
-    //how can i set it to have it so you only have to login once?
+ var user_name = prompt("Username: ");
+ var password = prompt("Password: ");
+ if(user_name == "Sammojr614" && password == "DevPassWord"){
+     alert("Welcome back, Sam!");
+ }
+ if(user_name == "Sammojr614" && password != "DevPassword"){
+     alert("Incorrect Password.");
+ }
+
+ if(user_name == "Holt" && password == "JamieRules"){
+     alert("Welcome Back, Jamison");
+ }
+}
