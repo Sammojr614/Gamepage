b063e2732fa88870c727cf9964ab9e2d46a17e68
Sammmojr <sammojr614@gmail.com>
2019-09-04 11:35:32 -0400

    Arrow Key Testing

--- docs/java/main.js
+++ docs/java/main.js
@@ -1,22 +1,9 @@
-
-var playerX;
-var playerY;
-var canvas = document.getElementById("gameTestCanvas");
-var ctx = canvas.getContext("2d");
-function player(playerX,playerY,color,height,width){
-    ctx.beginPath();
-    ctx.fillStyle = color;
-    ctx.fillRect(playerX,playerY,width,height);
-    ctx.stroke();
-}
 window.onload = function() {
-    var STARTING_X = 150;
-    var STARTING_Y = 150;
-    var playerOne = new player(STARTING_X,STARTING_Y,"red",150,150)
+    var canvas = document.getElementById("gameTestCanvas");
+    var ctx = canvas.getContext("2d");
     document.addEventListener("keydown", function(e){
         if(e.which == 37){
-            playerX -= 1;
+            alert("Hi!");
         }
     }, false)
 }
-
