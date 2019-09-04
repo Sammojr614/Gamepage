f79737c139190aaca025cf9e2cbdb9c6ad2ea536
Sammmojr <sammojr614@gmail.com>
2019-09-04 12:48:50 -0400

    Working on Key Tracking

--- docs/java/main.js
+++ docs/java/main.js
@@ -1,9 +1,31 @@
-window.onload = function() {
-    var canvas = document.getElementById("gameTestCanvas");
-    var ctx = canvas.getContext("2d");
+window.onload = function(){
     document.addEventListener("keydown", function(e){
-        if(e.which == 37){
-            alert("Hi!");
+        var canvas = document.getElementById("gameTestCanvas");
+        var ctx = canvas.getContext("2d");
+        //Up Arrow Key
+        if(e.which == 38){
+            ctx.font = "30px Arial";
+            ctx.fillText("Up",300,50);
         }
-    }, false)
+        //Down Arrow Key
+        if(e.which == 40){
+            ctx.font = "30px Arial";
+            ctx.fillText("Down",300,600);
+        }
+        //Right Arrow Key
+        if(e.which == 39){
+            ctx.font = "30px Arial";
+            ctx.fillText("Right",450,300);
+        }
+        //Left Arrow Key
+        if(e.which == 37){
+            ctx.font = "30px Arial";
+            ctx.fillText("Left",50,300);
+        }
+        if(e.which == 32){
+            ctx.clearRect(0,0,600,600);
+            alert("Space Key Pressed Clearing Canvas...");
+        }
+
+    })
 }
