458968eccb479f910f2721e3139cf968d8dba98f
Sammojr614 <Sammojr614@gmail.com>
2019-09-04 10:46:30 -0400

    working on game

--- docs/java/main.js
+++ docs/java/main.js
@@ -1,15 +1,22 @@
-window.onload = function() {
-    var canvas = document.getElementById("gameTestCanvas");
-    canvas.addEventListener("keydown", function(e){
-    ctx = canvas.getContext("2d");
 
-    function player(x,y,color,height,width){
-        ctx.beginPath();
-        ctx.fillStyle = color;
-        ctx.fillRect(x,y,width,height);
-        ctx.stroke();
-    }
-    var playerOne = new player(150,150,"red",150,150)
-    
-}, false)
+var playerX;
+var playerY;
+var canvas = document.getElementById("gameTestCanvas");
+var ctx = canvas.getContext("2d");
+function player(playerX,playerY,color,height,width){
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.fillRect(playerX,playerY,width,height);
+    ctx.stroke();
 }
+window.onload = function() {
+    var STARTING_X = 150;
+    var STARTING_Y = 150;
+    var playerOne = new player(STARTING_X,STARTING_Y,"red",150,150)
+    document.addEventListener("keydown", function(e){
+        if(e.which == 37){
+            playerX -= 1;
+        }
+    }, false)
+}
+
