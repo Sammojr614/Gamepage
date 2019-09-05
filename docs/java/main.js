d40ae3c4d27182d76f029e184e2bd7cf7a8a0a2f
Sammojr614 <Sammojr614@gmail.com>
2019-09-05 09:56:37 -0400

    Making Changes

--- docs/java/main.js
+++ docs/java/main.js
@@ -1,62 +1,51 @@
-window.onload = function() {
-    var prevPlayerX;
-    var prevPlayerY;
+window.onload = function(){
     var canvas = document.getElementById("gameTestCanvas");
     var ctx = canvas.getContext("2d");
     var playerX = 0;
     var playerY = 250;
-    function player(x,y, color, height, width){
+    function player(x,y,color,height,width){
         ctx.beginPath();
         ctx.fillStyle =color;
-        ctx.fillRect(x, y, width, height);
+        ctx.fillRect(x,y,height,width);
         ctx.stroke();
-    } 
-    
+    }
+    var playerOne = new player(playerX,playerY, "#00FF45",150,150);
+
     function moveLeft(){
-        var prevPlayerX = playerX+1;
         playerX -= 1;
-        ctx.clearRect(prevPlayerX,playerY,0,150,150);
         playerOne;
     }
     function moveUp(){
-        var prevPlayerY = playerY+1;
         playerY -= 1;
-        ctx.clearRect(playerX,prevPlayerY,150,150);
         playerOne;
     }
     function moveRight(){
-        var prevPlayerX = playerX-1;
         playerX += 1;
-        ctx.clearRect(prevPlayerX,playerY,150,150);
         playerOne;
     }
     function moveDown(){
-        var prevPlayerY = playerY-1;
         playerY += 1;
-        ctx.clearRect(playerX,prevPlayerY,150,150);
         playerOne;
     }
-    var playerOne = new player(playerX, playerY, "#00FF45", 150,150);
+
     document.addEventListener("keydown", function(e){
-        //Left Arrow key
+
+        //Left Arrow Key
         if(e.which == 37){
             moveLeft();
         }
         //Up Arrow Key
         if(e.which == 38){
-           moveUp();
+            moveUp();
         }
         //Right Arrow Key
         if(e.which == 39){
-           moveRight();
+            moveRight();
         }
-        //Down Arrow Key
+        //Down Arrow
         if(e.which == 40){
             moveDown();
         }
-        if(e.which == 32){
-            alert("clearing canvas");
-            ctx.clearRect(0,0,600,600);
-        }
+
     }, false)
 }
