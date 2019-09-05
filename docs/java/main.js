a2d53d19371db95c3c9d3cbebb065ab333cd4296
Sammojr614 <Sammojr614@gmail.com>
2019-09-05 09:27:40 -0400

    Game Progress, Square moving(not soild)

--- docs/java/main.js
+++ docs/java/main.js
@@ -1,31 +1,62 @@
-window.onload = function(){
+window.onload = function() {
+    var prevPlayerX;
+    var prevPlayerY;
+    var canvas = document.getElementById("gameTestCanvas");
+    var ctx = canvas.getContext("2d");
+    var playerX = 0;
+    var playerY = 250;
+    function player(x,y, color, height, width){
+        ctx.beginPath();
+        ctx.fillStyle =color;
+        ctx.fillRect(x, y, width, height);
+        ctx.stroke();
+    } 
+    
+    function moveLeft(){
+        var prevPlayerX = playerX+1;
+        playerX -= 1;
+        ctx.clearRect(prevPlayerX,playerY,0,150,150);
+        playerOne;
+    }
+    function moveUp(){
+        var prevPlayerY = playerY+1;
+        playerY -= 1;
+        ctx.clearRect(playerX,prevPlayerY,150,150);
+        playerOne;
+    }
+    function moveRight(){
+        var prevPlayerX = playerX-1;
+        playerX += 1;
+        ctx.clearRect(prevPlayerX,playerY,150,150);
+        playerOne;
+    }
+    function moveDown(){
+        var prevPlayerY = playerY-1;
+        playerY += 1;
+        ctx.clearRect(playerX,prevPlayerY,150,150);
+        playerOne;
+    }
+    var playerOne = new player(playerX, playerY, "#00FF45", 150,150);
     document.addEventListener("keydown", function(e){
-        var canvas = document.getElementById("gameTestCanvas");
-        var ctx = canvas.getContext("2d");
+        //Left Arrow key
+        if(e.which == 37){
+            moveLeft();
+        }
         //Up Arrow Key
         if(e.which == 38){
-            ctx.font = "30px Arial";
-            ctx.fillText("Up",300,50);
-        }
-        //Down Arrow Key
-        if(e.which == 40){
-            ctx.font = "30px Arial";
-            ctx.fillText("Down",300,600);
+           moveUp();
         }
         //Right Arrow Key
         if(e.which == 39){
-            ctx.font = "30px Arial";
-            ctx.fillText("Right",450,300);
+           moveRight();
         }
-        //Left Arrow Key
-        if(e.which == 37){
-            ctx.font = "30px Arial";
-            ctx.fillText("Left",50,300);
+        //Down Arrow Key
+        if(e.which == 40){
+            moveDown();
         }
         if(e.which == 32){
+            alert("clearing canvas");
             ctx.clearRect(0,0,600,600);
-            alert("Space Key Pressed Clearing Canvas...");
         }
-
-    })
+    }, false)
 }
