dd6f0c949a2c928d115cfea3566a1675dc272aa4
Sammojr614 <Sammojr614@gmail.com>
2019-09-10 09:08:53 -0400

    Another Refactor

--- docs/java/main.js
+++ docs/java/main.js
@@ -4,20 +4,19 @@ window.onload = function(){
     //Player Object Perameters
     player ={
         x: 0,
-        y: 550,
+        y:550,
+        color:"#00FD45",
         height:50,
         width:50,
-        color:"#00FD45",
         speed:12
     }
-    //Laser Object Perameters
-    pewpew ={
-        x: 0,
-        y: 575,
-        height:25,
-        width:50,
-        color:"00FD45",
-        speed:12
+    //Selector Perameters
+    selector ={
+        x: 250,
+        y: 325,
+        radius:5,
+        color:"lightGreen",
+        speed:25
     }
     //Draw Player
     function drawPlayer(player){
@@ -26,62 +25,99 @@ window.onload = function(){
         ctx.fillRect(player.x,player.y,player.width,player.height);
         ctx.stroke();
     }
-    //Draw the Laser
-    function drawLaser(pewpew){
+    //Drawing the Selector
+    function drawSelector(selector){
         ctx.beginPath();
-        ctx.fillStyle =pewpew.color;
-        ctx.fillRect(pewpew.x,pewpew.y,pewpew.width,pewpew.height);
+        ctx.fillStyle = selector.color;
+        ctx.fill();
+        ctx.arc(selector.x,selector.y,selector.radius,0, 2 * Math.PI);
         ctx.stroke();
     }
-    //Clearing Objects for Movement
+    //Clearing Objects
     function clearObject(player){
         ctx.clearRect(player.x,player.y,player.width,player.height);
     }
-    //Jumping Function
-    function jump(){
-        let start = Date.now;
-        let timer = setInterval(function(){
-            let timePassed = Date.now() - start;
-            if(timePassed >= 500){
-                clearInterval(timer);
-                return
-            }
-            draw(timePassed)
-        })
+    //Drawing the Text at the Beginning
+    function beginText(){
+        ctx.font = "30px Arial";
+        ctx.textAlign = "center";
+        ctx.fillStyle= "#00FF00"
+        ctx.fill();
+        ctx.fillText("Game Title", 300,300);
     }
-    document.addEventListener("keydown", function(e){
-        //The P key 
-        if(e.which == 80){
-            ctx.beginPath();
-            ctx.font = "30px Arial";
-            ctx.textAlign = "center";            
-            ctx.fillText("Game Start!",300,300);
-            ctx.stroke();
-
+    //Select Menu
+    function selectMenu(){
+        ctx.font = "20px Arial";
+        ctx.textAlign = "center";
+        ctx.fillStyle ="lightGreen";
+        ctx.fill();
+        ctx.fillText("Start", 300,350);
+        ctx.fillText("Exit", 300,375);
+    }
+    //Drawing the Full Menu
+    function drawMenu(){
+    beginText();
+    selectMenu();
+}
+function jump(){
+    let start = Date.now();
+    let timer = setInterval(function(){
+        let timePassed = Date.now(); - start;
+        if(timePassed >= 500){
+            clearInterval(timer);
+            return;
         }
-        //Down Arrow key
-        if(e.which == 40){
+        draw(timePassed);
+
+    },20)
+    function draw(timePassed){
+        clearObject(player);
+        player.y -= 50;
+        player.x += 50;
+        drawPlayer(player);
+    }
+}
+drawMenu();
+    document.addEventListener("keydown", function(e){
+        //Starting the Game
+        if(e.which == 13 && selector.y == 350){
+            alert("Game Start");
             ctx.clearRect(0,0,600,600);
             drawPlayer(player);
         }
-        //Left Arrow Key
+        //Exiting
+        if(e.which == 13 && selector.y == 375){
+            alert("Exiting Game");
+            ctx.clearRect(0,0,canvas.width,canvas.height);
+        }
+        //1 key
+        if(e.which == 49){
+            ctx.clearRect(selector.x,selector.y, 4,4);
+           selector.y = 350;
+           drawSelector(selector);
+        }
+        //2 key
+        if(e.which == 50){
+            ctx.clearRect(selector.x,selector.y, 4,4);
+           selector.y = 375;
+           drawSelector(selector);
+        }
+        //Right Arrow 
+        if(e.which == 39){
+            clearObject(player);
+            player.x += player.speed
+            drawPlayer(player);
+        }
+        //Left Arrow
         if(e.which == 37){
             clearObject(player);
             player.x -= player.speed;
-            pewpew.x = player.x;
             drawPlayer(player);
         }
-        //Right Arrow Key
-        if(e.which == 39){
-            clearObject(player);
-            player.x += player.speed;
-            pewpew.x = player.x;
-            drawPlayer(player);
-        }
-        //Space and Up Arrow Key
-        if(e.which == 32 || e.which == 38){
+        //Space and Up Arrow
+        if(e.which == 38 || e.which == 32){
             jump();
-            comeDown();
         }
     }, false)
+
 }
